d8e9153acc4d2d281b4e190a5ac78c7e2b2a202d
Steve Hellier <steve_hellier@hotmail.com>
2020-07-31 16:09:39 +0100

    Initial Commit

---
+++ commands/index.js
@@ -0,0 +1,18 @@
+const ping = require('./ping');
+const eightBall = require('./8ball');
+
+const commands = {
+  ping,
+  '8ball': eightBall,
+};
+
+/* eslint no-console: 0 */
+module.exports = async (msg) => {
+  const args = msg.content.split(' ');
+  console.log(args);
+  if (args.length === 0 || args[0].charAt(0) !== '!') return;
+  const command = args.shift().substr(1);
+  if (Object.keys(commands).includes(command)) {
+    commands[command](msg, args);
+  }
+};
